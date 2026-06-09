f664d943f39149d9d8bb73133777f0286c3e7030
Naran121x <116371253+Naran121x@users.noreply.github.com>
2026-06-09 09:19:50 +0545

    !!!!!!!!

---
+++ sd1.py
@@ -0,0 +1,19 @@
+# This is a side project of mine to learn amd uunderstand the fundamentals more clearly.
+name = input ("What do you want to play? ")
+name = name.lower()
+if name == "chess" or name == "checkers":
+    print ("Great choice! " + name + " is a fun game. It is a game of strategy and skill. I hope you enjoy playing it!")
+elif name == "cricket" or name == "football":
+    print ( name + "is a great sport which is played outside and is a way of getting exercise. I hope you have fun playing it!")
+else: 
+    print ("Do play some games bruhh")
+
+
+bored = input ("Anything new? Feel free to share it with me on insta ")
+bored = bored.lower()
+if bored == "yes":
+    print ("You want my insta nuh uhh but ya can msg me in disocrd if you want. Same username as here")
+elif bored == "no":
+    print ("Thats fine ig")
+
+# I mean I though I was gonna be formal but nahh thats too boring. Wanted it to be fun!! :) 
